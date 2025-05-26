b5d57be860fc9076fd705c3bb3b26c2226596180
MEDDADMakhlouf <makhloufmeddad@gmail.com>
2025-05-25 22:42:48 -0400

    Enhance login functionality and user experience; store username in localStorage, update greeting on dashboard, and improve styling.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,14 +6,15 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
+     const username=localStorage.getItem("username") || "";
     return (
         <div className="p-6 space-y-8">
             <div className="flex items-center justify-between pt-8">
                 <div>
-                    <h1 className="text-2xl font-bold">Hello Prof. Neila Hoacine! 👋</h1>
+                    <h1 className="text-2xl font-bold">Hello Prof.{username}  </h1>
                     <p className="text-gray-500">Welcome back! Here's your workspace.</p>
                 </div>
-                <UserAvatar name="Neila Hoacine" className="h-12 w-12" />
+                <UserAvatar name={username} className="h-12 w-12" />
             </div>
 
             <div className="space-y-2">
@@ -30,12 +31,12 @@ export default function HomePage() {
                 <div className="grid gap-4 md:grid-cols-2">
                     <SwapRequestNotification
                         type="new"
-                        message="Prof. Neila Hocini has requested to swap Physics 202 on March 26, 2025 for Computer Networks"
+                        message="Prof. wail has requested to swap Physics 202 on March 26, 2025 for Computer Networks"
                         time="10 minutes ago"
                     />
                     <SwapRequestNotification
                         type="rejected"
-                        message="Prof. Neila Hocini has has rejected your swap request for Computer Networks on March 2 2025"
+                        message="Prof. Nsalah has has rejected your swap request for Computer Networks on March 2 2025"
                         time="10 minutes ago"
                     />
                 </div>
